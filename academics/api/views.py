a757824ae05024b3bba2caf35f6990cd7a344281
AbdelRahman Tamer <abdulrahmantamer765@gmail.com>
2026-04-10 18:14:06 +0200

    Adding permission classes"

--- academics/api/views.py
+++ academics/api/views.py
@@ -1,20 +1,25 @@
 from rest_framework import viewsets
+from academics.api.permissions import IsAdminOrReadOnly
 from academics.models import Department, Discipline, Term, Course
 from .serializers import CourseSerializer, DepartmentSerializer, DisciplineSerializer, TermSerializer
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 class DisciplineViewSet(viewsets.ModelViewSet):
     queryset = Discipline.objects.all()
     serializer_class = DisciplineSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 class TermViewSet(viewsets.ModelViewSet):
     queryset = Term.objects.all()
     serializer_class = TermSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    permission_classes = [IsAdminOrReadOnly]
     
